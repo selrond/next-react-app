9cb96b6847b82d16a1a583d4f9f3dbe9449b59e2
Sebastian Andil <sebastian.andil@gmail.com>
2020-10-13 20:34:37 +0200

    Add react-router 6 & client side routing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-const HomePage = () => {
-  return <div>Welcome to Next.js!</div>
+/**
+ * Rendering null on the server and not on the client will produce
+ * a hydration mismatch warning.
+ * Since, in this case, that's expected, we can silence it
+ * with a quick wrapper component that will supress the warning
+ */
+const SafeHydrate = ({ children }) => {
+  return (
+    <div suppressHydrationWarning>
+      {typeof document === 'undefined' ? null : children}
+    </div>
+  )
 }
 
-export default HomePage
+const App = () => {
+  return (
+    <SafeHydrate>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <h1>Hello World!</h1>
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </SafeHydrate>
+  )
+}
+
+export default App
